a1f9034cf7b74a76c206e3d35355b3e72d7cb5f7
Alexandre <6315722@cegepoutaouais.qc.ca>
2026-08-31 21:18:09 -0400

    Premiere classe fini et diagramme de classe

---
+++ LaVieDeBob/Humain.cs
@@ -0,0 +1,17 @@
+﻿class humain
+{
+    public string Nom { get; set; }
+    public int Age { get; set; }
+    public float Capital; 
+    public humain(string nom, int age, float capital)
+    {
+        Nom = nom;
+        Age = age;
+        Capital = capital;
+    }
+    public void SePresenter()
+    {
+        Console.WriteLine($"Bonjour, je m'appelle {Nom} et j'ai {Age} ans. J'ai {Capital} dollars.");
+    }
+
+}
--- LaVieDeBob/Program.cs
+++ LaVieDeBob/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+class main
+{
+    static void Main(string[] args)
+    {
+        humain bob = new humain("Bob", 30, 1000.0f);
+        bob.SePresenter();
+
+    }
+}
